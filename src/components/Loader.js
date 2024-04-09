cf88fa91f9df8be19881e389ac3092fe34ed631d
Aditya Ningule <adiningule2@gmail.com>
2024-04-09 13:59:18 +0530

    Modal changes and loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,20 +1,20 @@
 import * as React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
-
 import { Container } from "react-bootstrap";
 
 const Loader = () => {
   return (
     <Container className="d-flex flex-column justify-content-center align-items-center">
-
-      <div>
-        <Box sx={{ display: "flex" }}>
-          <CircularProgress />
+      <div style={{ textAlign: "center" }}>
+        <Box sx={{ display: "flex", justifyContent: "center", marginBottom: "20px" }}>
+          <CircularProgress size={60} color="primary" />
         </Box>
+        <div style={{ fontSize: "18px", fontWeight: "bold", color: "#333", marginBottom: "10px" }}>
+          Hold on while we fetch records...
+        </div>
+        <div style={{ fontSize: "14px", color: "#666" }}>Please wait patiently</div>
       </div>
-      <div className="mt-1">Hold on while we fetch records...</div>
-
     </Container>
   );
 };
